a3fbe087cd30608131c9c842430f86fab27a5e08
Le Jossec Lukas <lejossec.lukas@gmail.com>
2022-11-09 13:49:38 +0000

    First version

---
+++ hello.scala
@@ -0,0 +1,18 @@
+object Hello {
+
+    def main(args: Array[String]) = {
+
+        println("Hello, world")
+	var var3=2
+	if(var3==1)
+	{
+		println("True")
+	}
+	else
+	{
+		println("False")
+	}
+
+    }
+
+}
