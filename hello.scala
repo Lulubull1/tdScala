744198cb81ad197b2bf882ba481aa20407717f13
Le Jossec Lukas <lejossec.lukas@gmail.com>
2022-11-14 15:32:41 +0000

    Thirst version

--- hello.scala
+++ hello.scala
@@ -16,18 +16,33 @@
 //
 //}
 
-//==================================PROGRAMME N°2 (14/11/22)==================================
-object Geeks
-{
-	//Main methods
-	def main(args: Array[String])
-	{
-		//Prints Hello world
-		println("Hello world by "+args(0))
-	}
-}
+//(14/11/22)
+//==================================PROGRAMME N°2==================================
+//object Geeks
+//{
+//	//Main methods
+//	def main(args: Array[String])
+//	{
+//		//Prints Hello world
+//		println("Hello world by "+args(0))
+//	}
+//}
 
 //scalac geeks.scala
 //Lancer le programme avec scala geeks.scala 'argument string a entrer'
 
 //scalac hello.scala
+
+//==================================PROGRAMME N°3==================================
+object Geeks
+{
+	//Main methods
+	def main(args: Array[String])
+	{
+		for(a<-1 to 10)
+			println("Valeur de a :"+a)
+
+		//Prints Hello world
+		println("Hello world by "+args(0))
+	}
+}
