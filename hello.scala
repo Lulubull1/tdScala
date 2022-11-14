47f3f6efcf709312ff8208184704c762efd3ac2b
Le Jossec Lukas <lejossec.lukas@gmail.com>
2022-11-14 15:21:08 +0000

    second version

--- hello.scala
+++ hello.scala
@@ -1,7 +1,6 @@
+//==================================PROGRAMME N°1==================================
 object Hello {
-
     def main(args: Array[String]) = {
-
         println("Hello, world")
 	var var3=2
 	if(var3==1)
@@ -16,3 +15,19 @@ object Hello {
     }
 
 }
+
+//==================================PROGRAMME N°2 (14/11/22)==================================
+object Geeks
+{
+	//Main methods
+	def main(args: Array[String])
+	{
+		//Prints Hello world
+		println("Hello world by "+args(0))
+	}
+}
+
+//scalac geeks.scala
+//Lancer le programme avec scala geeks.scala 'argument string a entrer'
+
+//scalac hello.scala
